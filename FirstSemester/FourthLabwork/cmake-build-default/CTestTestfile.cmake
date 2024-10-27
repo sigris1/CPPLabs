1aed71f1dc8cf243e1a3cadbdb3ffbf75b4c27e0
Kharev Yaroslav <144999355+sigris1@users.noreply.github.com>
2024-10-27 17:16:52 +0300

    Delete bad files

--- FirstSemester/FourthLabwork/cmake-build-default/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sigris/CPPLabs/FirstSemester/FourthLabwork
-# Build directory: /Users/sigris/CPPLabs/FirstSemester/FourthLabwork/cmake-build-default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("bin")
-subdirs("tests")
--- FirstSemester/FourthLabwork/cmake-build-default/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sigris/CPPLabs/FirstSemester/FourthLabwork/tests
-# Build directory: /Users/sigris/CPPLabs/FirstSemester/FourthLabwork/cmake-build-default/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/sigris/CPPLabs/FirstSemester/FourthLabwork/cmake-build-default/tests/argparser_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
